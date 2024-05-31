deaa5a199b44ec010c5bb13e044e6e837e7179a6
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-31 15:57:03 +0500

    login user controller & route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,8 @@
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+// Register User
 const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
@@ -24,4 +27,33 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+// Login User
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  res.status(200).json({
+    message: "Login successful",
+    token,
+  });
+};
+
+module.exports = { registerUser, loginUser };
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    fullname: {
       type: String,
       required: true,
     },
