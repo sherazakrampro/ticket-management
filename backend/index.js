ae884cb4f07c9e6e01bf51966dfd12749a3d930f
Sheraz Akram <sherazakram.pro@gmail.com>
2024-05-31 14:42:11 +0500

    register user route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const connectDB = require("./db/dbConnection");
+const userRouter = require("./routes/user.route");
 
 const app = express();
 
@@ -10,6 +11,8 @@ connectDB(mongodbURL);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/user", userRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
