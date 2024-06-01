4f6da9131d83e053577692fddd133bb23ac4ffe0
Sheraz Akram <sherazakram.pro@gmail.com>
2024-06-01 12:39:53 +0500

    user Slice

---
+++ frontend/src/redux/user/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: false,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    loginFailure: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure } = userSlice.actions;
+export default userSlice.reducer;
