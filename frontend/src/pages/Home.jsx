e1809ec7f112db0c802a8272b7ee9b58931d076a
Sheraz Akram <sherazakram.pro@gmail.com>
2024-06-02 13:24:33 +0500

    fetched all tickets on Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Home = () => {
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    const getAllTickets = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/tickets");
+        setTickets(response.data);
+      } catch (error) {
+        console.error("Error fetching tickets", error);
+      }
+    };
+
+    getAllTickets();
+  }, []);
+
   return (
     <div className="max-w-6xl mx-auto p-3">
-      <h1 className="text-4xl font-bold">Home Page</h1>
+      {tickets.length === 0 ? (
+        <p>No tickets available</p>
+      ) : (
+        tickets.map((ticket) => <div key={ticket._id}>{ticket.title}</div>)
+      )}
     </div>
   );
 };
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
-import ticketsReducer from "./tickets/ticketsSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  tickets: ticketsReducer,
 });
 
 const persistConfig = {
