c8564f66e3ac67a46cc80f99c1d044f3cedbdda5
Sheraz Akram <sherazakram.pro@gmail.com>
2024-06-03 10:56:51 +0500

    Create Ticket Form

--- frontend/src/pages/CreateTicket.jsx
+++ frontend/src/pages/CreateTicket.jsx
@@ -1,7 +1,66 @@
 const CreateTicket = () => {
   return (
-    <div className="max-w-6xl mx-auto p-3">
-      <h1 className="text-4xl font-bold">Create Ticket</h1>
+    <div className="max-w-lg mx-auto p-4">
+      <h1 className="text-4xl text-center font-bold my-4">Create a Ticket</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          className="bg-sky-200 p-3 rounded-lg"
+          type="text"
+          placeholder="Title"
+          id="title"
+          required
+        />
+        <textarea
+          className="bg-sky-200 p-3 rounded-lg"
+          type="text"
+          placeholder="Description"
+          id="description"
+          required
+        />
+        <select
+          className="bg-sky-200 p-3 rounded-lg"
+          name="status"
+          id="status"
+          required
+        >
+          <option value="" disabled selected>
+            Status
+          </option>
+          <option value="open">Open</option>
+          <option value="in-progress">In Progress</option>
+          <option value="closed">Closed</option>
+        </select>
+        <select
+          className="bg-sky-200 p-3 rounded-lg"
+          name="priority"
+          id="priority"
+          required
+        >
+          <option value="" disabled selected>
+            Priority
+          </option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+        <select
+          className="bg-sky-200 p-3 rounded-lg"
+          name="category"
+          id="category"
+          required
+        >
+          <option value="" disabled selected>
+            Category
+          </option>
+          <option value="bug">Bug</option>
+          <option value="feature-request">Feature Request</option>
+          <option value="support">Support</option>
+          <option value="improvement">Improvement</option>
+        </select>
+        <button className="bg-sky-700 text-white p-3 rounded-lg hover:bg-sky-800 uppercase disabled:opacity-80">
+          Create Ticket
+        </button>
+      </form>
     </div>
   );
 };
