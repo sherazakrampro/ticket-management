f90b73c802091f65ce2ef8e8d3d753d932265cbb
Sheraz Akram <sherazakram.pro@gmail.com>
2024-06-04 17:16:32 +0500

    Go Back on Ticket Page

--- frontend/src/pages/TicketPage.jsx
+++ frontend/src/pages/TicketPage.jsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -81,38 +81,45 @@ const TicketPage = () => {
   }
 
   return (
-    <div className="p-4 mt-8 max-w-lg w-full mx-auto bg-sky-300 border border-sky-200 rounded-lg shadow-lg">
-      <h1 className="text-3xl font-bold text-center mb-8">
-        {ticket.ticket.title}
-      </h1>
-      <p className="text-gray-700 mb-4">{ticket.ticket.description}</p>
-      <div className="flex justify-between items-center text-gray-700 mb-2">
-        <span className="font-semibold">Status:</span>
-        <span>{ticket.ticket.status}</span>
+    <>
+      <div className="p-4 mt-8 max-w-lg w-full mx-auto bg-sky-300 border border-sky-200 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold text-center mb-8">
+          {ticket.ticket.title}
+        </h1>
+        <p className="text-gray-700 mb-4">{ticket.ticket.description}</p>
+        <div className="flex justify-between items-center text-gray-700 mb-2">
+          <span className="font-semibold">Status:</span>
+          <span>{ticket.ticket.status}</span>
+        </div>
+        <div className="flex justify-between items-center text-gray-700 mb-2">
+          <span className="font-semibold">Priority:</span>
+          <span>{ticket.ticket.priority}</span>
+        </div>
+        <div className="flex justify-between items-center text-gray-700 mb-2">
+          <span className="font-semibold">Category:</span>
+          <span>{ticket.ticket.category}</span>
+        </div>
+        <div className="flex justify-between items-center mt-6">
+          <span
+            onClick={handleEditButtonClick}
+            className="font-semibold text-sm bg-sky-700 text-white py-2 px-4 rounded-lg hover:bg-sky-800 cursor-pointer"
+          >
+            Edit
+          </span>
+          <span
+            onClick={handleDeleteButtonClick}
+            className="font-semibold text-sm bg-red-700 text-white py-2 px-4 rounded-lg hover:bg-red-800 cursor-pointer"
+          >
+            Delete
+          </span>
+        </div>
       </div>
-      <div className="flex justify-between items-center text-gray-700 mb-2">
-        <span className="font-semibold">Priority:</span>
-        <span>{ticket.ticket.priority}</span>
-      </div>
-      <div className="flex justify-between items-center text-gray-700 mb-2">
-        <span className="font-semibold">Category:</span>
-        <span>{ticket.ticket.category}</span>
-      </div>
-      <div className="flex justify-between items-center mt-6">
-        <span
-          onClick={handleEditButtonClick}
-          className="font-semibold text-sm bg-sky-700 text-white py-2 px-4 rounded-lg hover:bg-sky-800 cursor-pointer"
-        >
-          Edit
-        </span>
-        <span
-          onClick={handleDeleteButtonClick}
-          className="font-semibold text-sm bg-red-700 text-white py-2 px-4 rounded-lg hover:bg-red-800 cursor-pointer"
-        >
-          Delete
-        </span>
-      </div>
-    </div>
+      <Link to="/">
+        <div className="mt-6 max-w-lg w-full mx-auto font-bold text-sky-700 hover:underline cursor-pointer">
+          Go Back
+        </div>
+      </Link>
+    </>
   );
 };
 
