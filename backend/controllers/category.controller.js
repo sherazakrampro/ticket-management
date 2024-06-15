2039e668c196b658370eb0af8707a0cf7f1bc725
Sheraz Akram <sherazakram.pro@gmail.com>
2024-06-15 12:41:20 +0500

    get single category controller & route

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -27,4 +27,14 @@ const getAllCategories = async (req, res) => {
   }
 };
 
-module.exports = { createCategory, getAllCategories };
+// Get Single Category
+const getCategory = async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    res.status(200).json({ category });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createCategory, getAllCategories, getCategory };
--- backend/routes/category.route.js
+++ backend/routes/category.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   createCategory,
   getAllCategories,
+  getCategory,
 } = require("../controllers/category.controller");
 
 const router = express.Router();
 
 router.post("/create", createCategory);
 router.get("/", getAllCategories);
+router.get("/:id", getCategory);
 
 module.exports = router;
